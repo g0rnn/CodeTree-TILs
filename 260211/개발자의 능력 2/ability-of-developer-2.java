7f5fb81bdfb9f87900153edfa0ebe76aa8995bc2
gyunho <124599614+g0rnn@users.noreply.github.com>
2026-02-11 10:29:16 +0900

    Title: 개발자의 능력 2, Time: -1ms, Memory: -1MB, Status: Compile Error - Codetree

---
+++ 260211/개발자의 능력 2/ability-of-developer-2.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+public class Main {
+    public static int[] arr = new int[6];
+    static int totalSum = 0;
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        for(int i = 0; i < 6; i++) {
+            arr[i] = sc.nextInt();
+            totalSum += arr[i];
+        }
+        
+        int minDiff = Integer.MAX_VALUE;
+        for (int i =0; i < 6; i++) {
+            for (int j = i+1; j < 6; j++) {
+
+                for (int k = 0; k < 6; k++)
+                    for (int l = k+1; l < 6; l++) {
+                        if (k == i || k == j || l == i || l == j) continue;
+                        minDiff = Math.min(minDiff, diff(i, j, k, l));
+                    }
+
+            }
+        }
+        System.out.println(minDiff);
+    }
+
+    private static int idff(int i, int j, int k, int l) {
+        int team1 = arr[i]+arr[j];
+        int team2 = arr[k]+arr[l];
+        int team3 = totalSum - team1-team2;
+
+        int ret = Math.abs(team1-team2);
+        ret = Math.max(ret, Math.abs(team2-team3));
+        ret = Math.max(ret, Math.abs(team3-team1));
+        return ret;
+    }
+}
