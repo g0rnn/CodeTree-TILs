430661de4dbea60a76ef17f170cae07028ce73b8
gyunho <124599614+g0rnn@users.noreply.github.com>
2026-02-19 14:28:04 +0900

    Title: 겹치지 않는 선분 2, Time: 132ms, Memory: 10MB, Status: Passed - Codetree

--- 260219/겹치지 않는 선분 2/line-segments-that-do-not-overlap-2.java
+++ 260219/겹치지 않는 선분 2/line-segments-that-do-not-overlap-2.java
@@ -14,7 +14,7 @@ public class Main {
         for (int i = 0; i < n; i++) {
 
             boolean flag = false;
-            for (int j = i+1; j < n; j++) {
+            for (int j = 0; j < n; j++) {
                 if (i == j) continue;
 
                 if (cross(x1, x2, i, j)) {
