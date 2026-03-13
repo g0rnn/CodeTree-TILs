79d28fe58183ca9b588cd37b5a5bb8515bfe2dd7
gyunho <124599614+g0rnn@users.noreply.github.com>
2026-03-13 13:48:26 +0900

    Title: 십자 모양 폭발, Time: 124ms, Memory: 10MB, Status: Wrong Answer - Codetree

---
+++ 260313/십자 모양 폭발/cross-shape-bomb.java
@@ -0,0 +1,61 @@
+import java.util.*;
+
+public class Main {
+    static int[][] offset = {{1, 0}, {0, 1}, {-1, 0}, {0, -1}};
+    static int n;
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        n = sc.nextInt();
+        int[][] grid = new int[n][n];
+        for (int i = 0; i < n; i++)
+            for (int j = 0; j < n; j++)
+                grid[i][j] = sc.nextInt();
+        int r = sc.nextInt()-1;
+        int c = sc.nextInt()-1;
+        
+        bomb(grid, r, c);
+        //for (int i=0; i < n; i++) System.out.println(Arrays.toString(grid[i]));
+        pull(grid);
+        StringBuilder sb= new StringBuilder();
+        for (int i =0; i < n; i++) {
+            for (int j = 0; j < n; j++)
+                sb.append(grid[i][j]).append(' ');
+            sb.append('\n');
+        }
+        System.out.println(sb);
+    }
+
+    private static void pull(int[][] grid) {
+        for (int col = 0; col < n; col++) {
+            int write = n-1;
+            for (int row = n-1; row >= 0; row--) {
+                if (grid[row][col] == 0) continue;
+                grid[write][col] = grid[row][col];
+                if (write != row) grid[row][col]=0;
+                write--;
+            }
+            while (write>=0) grid[write--][col] = 0;
+        }
+    }
+
+    private static void bomb(int[][] grid, int r, int c) {
+        int cnt = grid[r][c];
+        grid[r][c] = 0;
+
+        for (int d = 0; d < 4; d++) {
+            int range=cnt-1;
+            int nx = c + offset[d][0];
+            int ny = r + offset[d][1];
+
+            while (inRange(nx, ny) && range-->0) {
+                grid[ny][nx] = 0;
+                nx = c + offset[d][0];
+                ny = r + offset[d][1];
+            }
+        }
+    }
+
+    private static boolean inRange(int x, int y) {
+        return 0 <= x && x < n && 0 <= y && y < n;
+    }
+}
